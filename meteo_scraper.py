27695d48102995ead6a51249e516071e0f67f064
Jacek <mrozowsky.jacek@gmail.com>
2024-02-05 20:47:27 +0100

    Creating main file

---
+++ meteo_scraper.py
@@ -0,0 +1,14 @@
+import requests
+# import datetime
+# from datetime import date
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://pomiary.gdanskiewody.pl/home/rain"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+# print(soup)
+
+tables = pd.read_html(url)
+t = tables[0]
