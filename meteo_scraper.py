76952e833d8f74c0e5cacaff91390933fcfaf544
Jacek <mrozowsky.jacek@gmail.com>
2024-03-08 23:14:38 +0100

    small changes, with quite progress

--- meteo_scraper.py
+++ meteo_scraper.py
@@ -6,11 +6,24 @@ import pandas as pd
 
 url = "https://pomiary.gdanskiewody.pl/home/rain"
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup)
 
-tables = pd.read_html(url)
-t = tables[0]
-print(t.columns)
-print(type(list(t.columns)[0]))
+# dfs = pd.read_html(response.text)
+print(soup)
+
+table = soup.find_all('td')
+print(table)
+# tds = table.tr.find_all('td')
+# print(tds)
+# x = 0
+# while x != 41:
+#     for n, td in enumerate(tds):
+#         print(f"index {n}: {td.text.strip()}")
+#     x = x + 1
+
+# tables = pd.read_html(url)
+# t = tables[0]
+# print(t.columns)
+# print(type(list(t.columns)[0]))
+# print(tables[1])
+
